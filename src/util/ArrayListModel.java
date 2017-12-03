448984aa469acf54f98adfd257ad2bb491eabaab
udany <long.eagle.goodies@gmail.com>
2017-12-03 19:38:51 -0200

    ArrayListModel - Enhancements for adding and removing items, as well as setting and getting the list

--- src/util/ArrayListModel.java
+++ src/util/ArrayListModel.java
@@ -3,13 +3,14 @@ package util;
 import base.BaseModel;
 import base.BaseStore;
 import base.SyncedList;
+import lombok.Getter;
 
 import javax.swing.*;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class ArrayListModel<E> extends AbstractListModel<E> {
-    protected List<E> list;
+    @Getter protected List<E> list;
 
     public ArrayListModel(List<E> list){
         this.list = list;
@@ -23,5 +24,34 @@ public class ArrayListModel<E> extends AbstractListModel<E> {
     @Override
     public E getElementAt(int index) {
         return list.get(index);
+    }
+
+    public void setList(List<E> l){
+        list = l;
+        fireContentsChanged(this, 0, this.getSize());
+    }
+
+    public void add(E item){
+        int size = getSize();
+        list.add(item);
+        fireIntervalAdded(this, size, size);
+    }
+
+    public void add(List<E> items){
+        int size = getSize();
+        int size2 = items.size();
+
+        list.addAll(items);
+        fireIntervalAdded(this, size, size+size2-1);
+    }
+
+    public void remove(E item){
+        int idx = this.list.indexOf(item);
+        this.list.remove(item);
+
+        fireIntervalRemoved(this, idx, idx);
+    }
+    public void remove(List<E> items){
+        for (E item : items) remove(item);
     }
 }
